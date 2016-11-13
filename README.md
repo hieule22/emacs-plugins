c3290b3281d4490c2f3a6dc24d0e8182da69c94f
Hieu Le <trunghieule94@gmail.com>
2016-11-13 07:42:23 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# My Emacs Plugins
+
+## About
+
+This repository contains a collection of custom functions created to enrich
+my daily programming in Emacs. It also marks my first forays into the wonderful,
+if underappreciated, language that is Lisp. For this is after all a learning
+experience, the code will inevitably be littered with awkward syntax, contrived
+structures and silly bugs. As such, readers are welcome to critique, reach out
+and contribute to this growing project.
---
+++ count-words-buffer.el
@@ -0,0 +1,20 @@
+;;; my-emacs-plugins --- Summary
+
+;;; Commentary:
+;;; This function returns the word count in the current buffer.
+
+;;; Code:
+(defun count-words-buffer ( )
+  "Count the number of words in the current buffer;                             
+print a message in the minibuffer with the result."
+  (interactive)
+  (let ((count 0))
+    (save-excursion
+      (goto-char (point-min))
+      (while ( < (point) (point-max))
+        (forward-word 1)
+        (setq count (+ 1 count)))
+      (message "buffer contains %d words." count))))
+
+(provide 'count-words-buffer)
+;;; count-words-buffer.el ends here
