3bc21020655f55c4c48343d9b3bc9f0713266016
Hieu Le <trunghieule94@gmail.com>
2016-11-16 09:22:10 -0600

    Fix remove-trailing-whitespace

--- lib/remove-trailing-whitespaces.el
+++ lib/remove-trailing-whitespaces.el
@@ -3,8 +3,14 @@
 ;;; Commentary:
 ;; This module highlights and removes (if prompted) trailing whitespaces
 ;; in a buffer.
+;; TODO(hieule): Remove trailing blank lines.
 
 ;;; Code:
+(defun move-end-of-line-before-whitespace ()
+  "Move point to the last non-whitespace character in the current line."
+  (move-end-of-line nil)
+  (re-search-backward "^\\|[^[:space:]]"))
+
 (defun remove-trailing-whitespaces ( )
   "Highlight and remove trailing whitespaces in current buffer;
 press space to remove and delete to skip."
@@ -12,17 +18,17 @@ press space to remove and delete to skip."
   (save-excursion
     (goto-char (point-min))
     (while ( < (point) (point-max))
-      (move-end-of-line 1)
-      (let* ((end (point)) (begin end))
-	;; (while ( = (buffer-substring begin (+ 1 begin)) " ")
-	;;   (setq begin (- 1 begin)))
-	(set-mark begin)
-	(move-end-of-line 1)
-	(if (y-or-n-p "Delete? ")
-	    (progn
-	      (delete-region (+ 1 begin) (+ 1 end))
-	      )))
+      (move-end-of-line-before-whitespace)
+      ;; Move point to first trailing whitespace of current line, if any.
+      (if (not (eolp))
+	  (forward-char))
+      (if (not (eolp))
+	  (progn
+	    (set-mark (point))
+	    (move-end-of-line 1)
+	    (if (y-or-n-p "Delete? ")
+		(backward-delete-char-untabify 1))))
       (forward-line 1))))
-
+ 
 (provide 'remove-trailing-whitespaces)
 ;;; remove-trailing-whitespaces.el ends here
